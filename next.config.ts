7845206bdab727c823f1ff01b364ca850e737db9
mdimran29 <imranansari2932003@gmail.com>
2026-04-03 02:35:34 +0530

    file rearranging

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    // silence root inference warnings since project was moved into 'frontend' dir
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
